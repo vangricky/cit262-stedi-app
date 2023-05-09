5d71a080df5222e847c56fe5d49804dc71fd59b1
Ricky Vang <vangcheemeng@yahoo.com>
2023-05-09 00:18:55 -0600

    comment deleted

--- screens/Login.js
+++ screens/Login.js
@@ -53,7 +53,7 @@ const Login = ({loggedInState, loggedInStates,setLoggedInState})=>{
             <TouchableOpacity
                style={styles.sendButton}
               onPress={async ()=>{
-                console.log(phoneNumber+' Button was pressed') //test comment
+                console.log(phoneNumber+' Button was pressed')
     
                 const sendTextResponse=await fetch(
                   'https://dev.stedi.me/twofactorlogin/'+phoneNumber,
